bff8ffadf5035625f1047f385162d8375e6fbfc7
ky0613 <95160084+ky0613@users.noreply.github.com>
2022-07-16 17:32:25 +0900

    Pageの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/User/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-// import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "こうせい",
-  image: "https://source.unsplash.com/7AIDE8PrvA0",
-  email: "12345@example.com",
-  phone: "090-1111-1212",
-  company: {
-    name: "ほげほげ株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout className="App">
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
