147cde162f7b41916efd133fe888d3bda3083f7f
ky0613 <95160084+ky0613@users.noreply.github.com>
2022-07-16 16:28:35 +0900

    Organismの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/User/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "こうせい",
+  image: "https://source.unsplash.com/7AIDE8PrvA0",
+  email: "12345@example.com",
+  phone: "090-1111-1212",
+  company: {
+    name: "ほげほげ株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
-      <SearchInput></SearchInput>
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
