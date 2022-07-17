4fe887c91611107d8b9dfe53aeb7df55982b082f
ky0613 <95160084+ky0613@users.noreply.github.com>
2022-07-17 12:44:34 +0900

    Contextでの再レンダリングの最適化

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,11 +1,12 @@
-import { useContext } from "react";
+import { memo, useContext } from "react";
 import styled from "styled-components";
 import { UserContext } from "../../../providers/UserProvider";
 
-export const UserIconWithName = (props) => {
+export const UserIconWithName = memo((props) => {
   const { image, name } = props;
   const { userInfo } = useContext(UserContext);
   const isAdmin = userInfo ? userInfo.isAdmin : false;
+  console.log("UserIconWithName");
 
   return (
     <SContainer>
@@ -14,7 +15,7 @@ export const UserIconWithName = (props) => {
       {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
-};
+});
 
 const SContainer = styled.div`
   text-align: center;
